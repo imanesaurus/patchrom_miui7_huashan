eebf7b08782c4fa91b07c6fc660ea6dc64cd6912
imanesaurus <pirmansah.otoluwa@gmail.com>
2015-10-21 10:04:47 +0800

    miui7

---
+++ phoneapps.mk
@@ -0,0 +1,26 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := Bluetooth \
+		BluetoothExt \
+		CertInstaller \
+		FM2 \
+		FMRecord \
+		HTMLViewer \
+		KeyChain \
+		PacProcessor \
+		UserDictionaryProvider \
+		WAPPushManager
+
+private-phone-priv-apps := BackupRestoreConfirmation \
+		DefaultContainerService \
+		FusedLocation \
+		InputDevices \
+		ExternalStorageProvider \
+		MediaProvider \
+		OneTimeInitializer \
+		ProxyHandler \
+		SharedStorageBackup \
+		Shell \
+		Tag \
+		VpnDialogs
